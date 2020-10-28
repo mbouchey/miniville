360575c730ed073cbbbf40d09fc13f3a6af0b13e
DarkHorion <maxime.boucheyrou@hotmail.fr>
2020-10-28 14:46:42 +0100

    Ajout des classes

---
+++ Pile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MiniVille
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Game g = new Game();
+            
+        }
+    }
+}
