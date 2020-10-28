4e8d82c5e043fff223d3a54032f9d353ffc49142
KelyosN <solal.kelyos@gmail.com>
2020-10-28 16:24:59 +0100

    Card Struct and Pile class created

---
+++ MiniVille/CardsInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniVille
+{
+    public struct CardsInfo
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Cost { get; set; }
+        public string Name { get; set; }
+        public string Effect { get; set; }
+        public int Dice { get; set; }
+        public int Gain { get; set; }
+    }
+}
--- MiniVille/Pile.cs
+++ MiniVille/Pile.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MiniVille
 {
 
     class Pile
     {
+        public List<CardsInfo> Available_cards { get; private set; }
 
+        public Pile(List<CardsInfo> available_cards)
+        {
+            this.Available_cards = available_cards;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public Card SellCard()
+        {
+            
+        }
+
+        //Show All cards with their information IN THE CONSOLE
+        public void DisplayShop()
+        {
+
+        }
     }
 }
