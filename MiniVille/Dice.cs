58ef335d0f1e028e2c1739c810685527fa66ffe1
DESKTOP-17EEK5K\Extar Grace <extargrace@gmail.com>
2020-10-28 16:53:51 +0100

    Dice Fait

--- MiniVille/Dice.cs
+++ MiniVille/Dice.cs
@@ -5,6 +5,22 @@ namespace MiniVille
 
     class Dice
     {
+        public int NbFaces;
+        public Random random = new Random();
+        public int face;
 
+        public Dice(): this(6)
+        {
+            //NbFaces = 6;
+        }
+        public Dice(int NbFaces)
+        {
+            this.NbFaces = NbFaces;
+        }
+        public virtual void Lancer()
+        {
+            int rdm = random.Next(1, NbFaces + 1);
+            face = rdm;
+        }
     }
 }
