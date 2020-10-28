d6b2d176a2be1ae6b88182c4dec70ed038fe8d81
DarkHorion <maxime.boucheyrou@hotmail.fr>
2020-10-28 16:49:43 +0100

    diagramme card converti en code

--- MiniVille/Card.cs
+++ MiniVille/Card.cs
@@ -5,6 +5,30 @@ namespace MiniVille
 
     class Card
     {
+        private int _price;
+        public int price {get {return _price;}}
 
+        private int _earning_money;
+        public int earning_money {get {return _earning_money;}}
+
+        private string _name;
+        public string name {get {return _name;}}
+
+        private int _activation_value;
+        public int activation_value {get {return _activation_value;}}
+
+        public Card(string name, int price, int earning_money, int activation_value)
+        {
+            this._name = name;
+            this._price = price;
+            this._earning_money = earning_money;
+            this._activation_value = activation_value;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("name:{0}|price:{1}|earning_money:{2}|activation_value:{3}\n", this.name, this.price, this.earning_money, this.activation_value);
+
+        }
     }
 }
