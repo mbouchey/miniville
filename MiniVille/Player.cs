46fdd729c702717467c1ac9af571d573d930397b
Lenovo-PC\Anthony <anthony.masq@free.fr>
2020-10-28 16:48:51 +0100

    player jsp quoi

--- MiniVille/Player.cs
+++ MiniVille/Player.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
 
 namespace MiniVille
 {
 
     class Player
     {
+        public List<Card> cards;
+        public int money;
+    }
 
+    public int EarnMoney()
+    {
+        
+    }
+
+    public int BuyCard()
+    {
+
+    }
+
+    public override string ToString()
+    {
+        string toString = String.Format("", this.);
+        toString += String.Format("", this.);
+        return toString;
     }
 }
