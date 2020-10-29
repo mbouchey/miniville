3176b1bce4d2d92e8c417962eea24004d7c405d6
DarkHorion <maxime.boucheyrou@hotmail.fr>
2020-10-29 11:28:59 +0100

    refonte card

--- MiniVille/Card.cs
+++ MiniVille/Card.cs
@@ -5,21 +5,25 @@ namespace MiniVille
 
     class Card
     {
+        private string _name;
+        public string name {get {return _name;}}
+
+        private string _color;
+        public string color {get {return _color;}}
+
         private int _price;
         public int price {get {return _price;}}
 
         private int _earning_money;
         public int earning_money {get {return _earning_money;}}
 
-        private string _name;
-        public string name {get {return _name;}}
-
         private int _activation_value;
         public int activation_value {get {return _activation_value;}}
 
-        public Card(string name, int price, int earning_money, int activation_value)
+        public Card(string name, string color, int price, int earning_money, int activation_value)
         {
             this._name = name;
+            this._color = color;
             this._price = price;
             this._earning_money = earning_money;
             this._activation_value = activation_value;
@@ -27,7 +31,7 @@ namespace MiniVille
 
         public override string ToString()
         {
-            return String.Format("name:{0}|price:{1}|earning_money:{2}|activation_value:{3}\n", this.name, this.price, this.earning_money, this.activation_value);
+            return String.Format("name:{0}|color:{1}|price:{2}|earning_money:{3}|activation_value:{4}\n", this.name, this.color, this.price, this.earning_money, this.activation_value);
 
         }
     }
