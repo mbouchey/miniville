77535d82af43c8234527b42744189e3e391f2fae
KelyosN <solal.kelyos@gmail.com>
2020-10-30 10:02:47 +0100

    Game constructor created

--- MiniVille/Game.cs
+++ MiniVille/Game.cs
@@ -12,6 +12,7 @@ namespace MiniVille
 
         public Game()
         {
+            //Cartes
             List<Card> cards = new List<Card>();
 
             cards.Add(new Card("Champs de blé","blue",1,1,1));
@@ -20,8 +21,21 @@ namespace MiniVille
             cards.Add(new Card("Café","Red",2,1,3));
             cards.Add(new Card("Superette","Green",2,3,4));
             cards.Add(new Card("Forêt","Blue",2,1,5));
-            cards.Add(new Card("Restaurant","Red",4,2,5)) ;
+            cards.Add(new Card("Restaurant","Red",4,2,5));
             cards.Add(new Card("Stade","Blue",6,4,6));
+
+            //Joueurs
+            players.Add(new Player());
+            players.Add(new Player());
+
+            //Dés
+            dices.Add(new Dice());
+
+            Console.WriteLine("Voulez-vous jouer avec deux dés ? (oui - non)");
+            if (Console.ReadLine() == "oui")
+            {
+                dices.Add(new Dice());
+            }
         }
 
         public void RunGame()
