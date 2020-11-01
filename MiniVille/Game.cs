6ac4404b9d8f986bc6261af2647bf40630383d4a
KelyosN <solal.kelyos@gmail.com>
2020-11-01 15:48:45 +0100

    Player Class changed

--- MiniVille/Game.cs
+++ MiniVille/Game.cs
@@ -14,20 +14,27 @@ namespace MiniVille
         public Game()
         {
             //Cartes
-            List<Card> cards = new List<Card>();
+            Dictionary<string, Card> cards = new Dictionary<string, Card>();
 
-            cards.Add(new Card("Champs de blé","blue",1,1,1));
-            cards.Add(new Card("Ferme","Blue",2,1,1));
-            cards.Add(new Card("Boulangerie","Green",1,2,2));
-            cards.Add(new Card("Café","Red",2,1,3));
-            cards.Add(new Card("Superette","Green",2,3,4));
-            cards.Add(new Card("Forêt","Blue",2,1,5));
-            cards.Add(new Card("Restaurant","Red",4,2,5));
-            cards.Add(new Card("Stade","Blue",6,4,6));
+            cards.Add("Champs de blé", new Card("Champs de blé","Blue",1,1,1));
+            cards.Add("Ferme", new Card("Ferme","Blue",2,1,1));
+            cards.Add("Boulangerie", new Card("Boulangerie","Green",1,2,2));
+            cards.Add("Café", new Card("Café","Red",2,1,3));
+            cards.Add("Superette", new Card("Superette","Green",2,3,4));
+            cards.Add("Forêt", new Card("Forêt","Blue",2,1,5));
+            cards.Add("Restaurant", new Card("Restaurant","Red",4,2,5));
+            cards.Add("Stade", new Card("Stade","Blue",6,4,6));
+
+            //Listes de cartes pour les joueurs au débuts de jeu
+            List<Card> starterCards = new List<Card>
+            {
+                cards["Champs de blé"],
+                cards["Boulangerie"]
+            };
 
             //Joueurs
-            players.Add(new Player());
-            players.Add(new Player());
+            players.Add(new Player(starterCards, "Joueur_1"));
+            players.Add(new Player(starterCards, "Joueur_2"));
 
             //Dés
             dices.Add(new Dice());
--- MiniVille/Player.cs
+++ MiniVille/Player.cs
@@ -8,23 +8,86 @@ namespace MiniVille
     class Player
     {
         public List<Card> cards;
+        public string name;
         public int money;
-    }
 
-    public int EarnMoney()
-    {
-        
-    }
+        public Player(List<Card> cards, string name)
+        {
+            this.cards = cards;
+            this.name = name;
+            this.money = 3;
+        }
 
-    public int BuyCard()
-    {
+        public void CheckCards(int dice, bool isPlaying, Player currentPlayer)
+        {
+            foreach (Card card in cards)
+            {
+                if (card.activation_value == dice)
+                {
+                    switch (card.color)
+                    {
+                        case "Blue":
+                            Console.WriteLine("La carte Bleu \"{0}\" de {1} s'active.", card.name, name);
+                            EarnMoney(card.earning_money);
+                            break;
 
-    }
+                        case "Green":
+                            if (isPlaying)
+                            {
+                                Console.WriteLine("La carte Verte \"{0}\" de {1} s'active.", card.name, name);
+                                EarnMoney(card.earning_money);
+                            }
+                            break;
 
-    public override string ToString()
-    {
-        string toString = String.Format("", this.);
-        toString += String.Format("", this.);
-        return toString;
+                        case "Red":
+                            if (!isPlaying)
+                            {
+                                Console.WriteLine("La carte Rouge \"{0}\" de {1} s'active.", card.name, name);
+                                int gainedMoney = currentPlayer.LooseMoney(card.earning_money);
+                                EarnMoney(gainedMoney);
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+
+        public int BuyCard()
+        {
+            return 1;
+        }
+
+        public void EarnMoney(int amount)
+        {
+            money += amount;
+
+            Console.WriteLine("{0} a gagné {1} pièces et a maintenant {2} pièces !", name, amount, money);
+        }
+
+        public int LooseMoney(int amount)
+        {
+            int oldMoney = money;
+            if (money == 0)
+            {
+                Console.WriteLine("{0} a déjà perdu tout ses pièces.", name);
+            }
+            else if (money - amount <= 0)
+            {
+                money = 0;
+                Console.WriteLine("{0} a perdu tout ses pièces.", name);
+            }
+            else
+            {
+                money -= amount;
+                Console.WriteLine("{0} a perdu {1} pièces et a maintenant {2} pièces.", name, amount, money);
+            }
+
+            return oldMoney - money;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
